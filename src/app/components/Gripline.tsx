f5ace9d8d66fd0b1935039d31e3ee3c0f000026d
Nicholas Miyasato <nicholetas@gmail.com>
2026-01-10 19:30:45 -0300

    updated all packages

--- src/app/components/Gripline.tsx
+++ src/app/components/Gripline.tsx
@@ -1,7 +1,7 @@
 const Gripline = () => {
     return ( 
         <svg width="" height="200px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 10H20M4 14H20" stroke="#008B8B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4 10H20M4 14H20" stroke="#008B8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
      );
 }
